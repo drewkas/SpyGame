26b40d047315497ae4d3aa045e07529cc7a9e94f
drewkas <57570992+drewkas@users.noreply.github.com>
2026-08-07 19:45:53 -0400

    Update app_test.js

--- app_test.js
+++ app_test.js
@@ -7,10 +7,10 @@ alert("app.js loaded");
 const SUPABASE_URL = "https://cevpdsrjsqavrrtlpyoa.supabase.co/rest/v1/";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNldnBkc3Jqc3FhdnJydGxweW9hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxMzE3NTUsImV4cCI6MjEwMTcwNzc1NX0.nl5HKXm2AOcQYFDSQARmcRVXvCRe9cf32OEj3P5Jk6w";
 
-//const supabase = window.supabase.createClient(
+const supabase = window.supabase.createClient(
 //    SUPABASE_URL,
 //    SUPABASE_KEY
-//);
+);
 
 // =====================================================
 // GLOBAL VARIABLES
