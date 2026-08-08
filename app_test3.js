d086083971a037341403f1c48c91746cb568e6f8
drewkas <57570992+drewkas@users.noreply.github.com>
2026-08-07 22:01:49 -0400

    Update app_test3.js

--- app_test3.js
+++ app_test3.js
@@ -12,15 +12,23 @@ const SUPABASE_URL = "https://cevpdsrjsqavrrtlpyoa.supabase.co/rest/v1/";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNldnBkc3Jqc3FhdnJydGxweW9hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxMzE3NTUsImV4cCI6MjEwMTcwNzc1NX0.nl5HKXm2AOcQYFDSQARmcRVXvCRe9cf32OEj3P5Jk6w";
 
 try {
-    const supabase =
+    //const supabase =
+    //    window.supabase.createClient(
+    //        SUPABASE_URL,
+    //        SUPABASE_KEY
+    //    );
+
+    const supabaseClient =
         window.supabase.createClient(
             SUPABASE_URL,
             SUPABASE_KEY
         );
 
-    console.log("Supabase client created");
-    console.log("supabase =", window.supabase);
-    console.log("supabase =", supabase);
+    console.log(supabaseClient);
+
+    //console.log("Supabase client created");
+    //console.log("supabase =", window.supabase);
+    //console.log("supabase =", supabase);
 
 } catch (err) {
     console.error(err);
