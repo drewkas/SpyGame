13e7e48f35b937df7b75ced788844ee18aa7b399
drewkas <57570992+drewkas@users.noreply.github.com>
2026-08-07 21:29:48 -0400

    Update app_test3.js

--- app_test3.js
+++ app_test3.js
@@ -12,8 +12,6 @@ console.log("window.supabase =", window.supabase);
 const SUPABASE_URL = "https://cevpdsrjsqavrrtlpyoa.supabase.co/rest/v1/";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNldnBkc3Jqc3FhdnJydGxweW9hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxMzE3NTUsImV4cCI6MjEwMTcwNzc1NX0.nl5HKXm2AOcQYFDSQARmcRVXvCRe9cf32OEj3P5Jk6w";
 
-console.log(window.supabase);
-
 try {
 
     const supabase =
