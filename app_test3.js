c07a933e28ecd699f705b2cb72966e28c9d2bb74
drewkas <57570992+drewkas@users.noreply.github.com>
2026-08-07 21:32:18 -0400

    Update app_test3.js

--- app_test3.js
+++ app_test3.js
@@ -13,7 +13,6 @@ const SUPABASE_URL = "https://cevpdsrjsqavrrtlpyoa.supabase.co/rest/v1/";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNldnBkc3Jqc3FhdnJydGxweW9hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYxMzE3NTUsImV4cCI6MjEwMTcwNzc1NX0.nl5HKXm2AOcQYFDSQARmcRVXvCRe9cf32OEj3P5Jk6w";
 
 try {
-
     const supabase =
         window.supabase.createClient(
             SUPABASE_URL,
@@ -23,9 +22,7 @@ try {
     console.log("Supabase client created");
 
 } catch (err) {
-
     console.error(err);
-
 }
 
 // =====================================================
